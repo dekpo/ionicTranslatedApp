ccc229b03e2824f7f7fab2b83e9262826187cfb7
Dekpo <dekpo@me.com>
2021-04-30 16:08:02 +0200

    Added the Language Service logic

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { TranslateService } from '@ngx-translate/core';
+
+const LNG_KEY = 'SELECTED_LANGUAGE';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
+  selected = '';
 
-  constructor() { }
+  constructor(private translate: TranslateService, private storage: Storage) { }
+
+  setInitialAppLanguage() {
+    let language = this.translate.getBrowserLang();
+    this.translate.setDefaultLang(language);
+
+    this.storage.get(LNG_KEY).then(val => {
+      if (val) {
+        this.setLanguage(val);
+        this.selected = val;
+      }
+    });
+  }
+
+  getLanguages() {
+    return [
+      { text: 'English', value: 'en', img: 'assets/imgs/en.png' },
+      { text: 'German', value: 'de', img: 'assets/imgs/de.png' },
+    ];
+  }
+
+  setLanguage(lng) {
+    this.translate.use(lng);
+    this.selected = lng;
+    this.storage.set(LNG_KEY, lng);
+  }
 }
